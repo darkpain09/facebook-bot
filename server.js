734ce6971dab9b1665af119513ce03d318c9c1ee
darkpain09 <73021515+darkpain09@users.noreply.github.com>
2020-10-19 12:06:48 +0800

    Update server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+import * as express from "express";
+import morgan from 'morgan';
+import { urlencoded, json } from 'body-parser';
+const app = express();
+// app configuration
+app.set('port', (process.env.PORT || 5000));
+// setup our express application
+app.use(morgan('dev')); // log every request to the console.
+app.use(urlencoded({ extended:false }));
+app.use(json()); 
+// app routes
+require('./routes/webhook_verify')(app);
+// warming up the engines !! setta !! go !!!.
+app.listen(app.get('port'), function() {
+  const url = 'http://localhost:' + app.set('port');
+  console.log('Application running on port: ', app.get('port'));
+});
